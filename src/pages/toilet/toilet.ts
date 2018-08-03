774af115d4ef75860d70b4e3a6f3b340199d523b
Timon Riemslagh <timon.riemslagh@wijs.be>
2018-08-03 15:25:17 +0200

    Toilet first version.

--- src/pages/toilet/toilet.ts
+++ src/pages/toilet/toilet.ts
@@ -7,12 +7,30 @@ import {NavController} from "ionic-angular";
 })
 export class ToiletPage {
 
+    public male = true;
+    public female = true;
+
     constructor(public navCtrl: NavController) {
 
     }
 
     goBack() {
         this.navCtrl.pop();
+    }
+
+    selectAll() {
+        this.male = true;
+        this.female = true;
+    }
+
+    selectMan() {
+        this.male = true;
+        this.female = false;
+    }
+
+    selectWoman() {
+        this.male = false;
+        this.female = true;
     }
 
 }
